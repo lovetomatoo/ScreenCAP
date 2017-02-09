720d5e69153c2731ff6bf7b63e019a8bb97c8cd8
lovetomatoo <736685433@qq.com>
2017-02-09 18:29:58 +0800

    COMMIT
    COMMIT

--- ScreenCAP/app/src/main/java/com/quanmin/guohongxin/screencap/MainActivity.java
+++ ScreenCAP/app/src/main/java/com/quanmin/guohongxin/screencap/MainActivity.java
@@ -1,26 +1,35 @@
 package com.quanmin.guohongxin.screencap;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.PixelFormat;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
-import android.media.Image;
-import android.media.ImageReader;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
+import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
+@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String TAG = getClass().getSimpleName();
@@ -32,6 +41,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private VirtualDisplay mVirtualDisplay;
     private SurfaceView mSvMain;
     private int mDensityDpi;
+
+    private static final String MIME_TYPE = "video/avc"; // H.264 Advanced Video Coding
+    private MediaCodec mCodec;
+    private Surface mInputSurface;
+
+    private MediaCodec.BufferInfo mBufferInfo = new MediaCodec.BufferInfo();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -50,6 +70,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBtnCap.setOnClickListener(this);
 
         mSvMain = (SurfaceView) findViewById(R.id.sv_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -71,23 +94,83 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return;
         }
 
+        try {
+            prepareEncoder();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        Surface surface = creatSurface();
+
         mVirtualDisplay = mediaProjection.createVirtualDisplay(TAG,
                 mSvMain.getWidth(),
                 mSvMain.getHeight(),
                 mDensityDpi,
                 DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                creatSurface(), null, null);
+                mInputSurface, null, null);
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private Surface creatSurface() {
-//        ImageReader imageReader = ImageReader.newInstance(500, 500, PixelFormat.RGBA_8888, 1);
-//        image = imageReader.acquireLatestImage();
-//
-//        mSurface = imageReader.getSurface();
 
         Surface surface = mSvMain.getHolder().getSurface();
 
         return surface;
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void prepareEncoder() throws IOException {
+
+        MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, 500, 500);
+        format.setInteger(MediaFormat.KEY_COLOR_FORMAT,
+                MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        format.setInteger(MediaFormat.KEY_BIT_RATE, 6000000);
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, 30);
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 10);
+
+        Log.d(TAG, "created video format: " + format);
+        mCodec = MediaCodec.createEncoderByType(MIME_TYPE);
+        mCodec.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        mInputSurface = mCodec.createInputSurface();
+        Log.d(TAG, "created input surface: " + mInputSurface);
+        mCodec.start();
+
+        //
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                resetOutputFormat();
+            }
+        }).start();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void resetOutputFormat() {
+
+        MediaFormat outputFormat = mCodec.getOutputFormat(); // 方式二
+        for (; ; ) {
+            int outputBufferId = mCodec.dequeueOutputBuffer(mBufferInfo, 10000);
+            if (outputBufferId >= 0) {
+                ByteBuffer outputBuffer = mCodec.getOutputBuffer(outputBufferId);
+                // 方式一
+                MediaFormat bufferFormat = mCodec.getOutputFormat(outputBufferId);
+                mCodec.releaseOutputBuffer(outputBufferId, false);
+
+            } else if (outputBufferId == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                outputFormat = mCodec.getOutputFormat(); // 方式二
+            }
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mCodec.stop();
+        mCodec.release();
     }
 }
